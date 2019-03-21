3db101db53e3ace850f3d908210f822c3f72ce35
Verotredo <niktesla96@gmail.com>
2019-03-21 06:31:26 -0700

    hw5err5

--- src/SM.ml
+++ src/SM.ml
@@ -30,24 +30,36 @@ type config = int list * Stmt.config
 let rec eval env scfg prog =
     let (st, cfg) = scfg     in
     let (s, i, o) = cfg in
-    match prog with
-    | []            -> scfg
-    | BINOP op :: p ->
-        let y :: x :: st1 = st in
-        let res = Expr.eval s (Binop (op, Const x, Const y))
-        in eval (res :: st1, cfg) p
-    | CONST c  :: p -> eval (c :: st, cfg) p
-    | READ     :: p -> eval ((List.hd i) :: st, (s, List.tl i, o)) p
-    | WRITE    :: p -> eval (List.tl st, (s, i, o @ [List.hd st])) p
-    | LD x     :: p -> eval (s x :: st, cfg) p
-    | ST x     :: p -> eval (List.tl st, (Expr.update x (List.hd st) s, i, o)) p 
-    | LABEL _ :: p -> eval env scfg p
-    | JMP l ::p -> eval env scfg (env#labeled l)
-    | CJMP (m, l)::next ->
-        let x::st1 = st in
-        let goto = (env#labeled l) in
-        let tg = if ((m="z") && (x == 0)|| x != 0 && m = "nz") then goto else next in
-        eval env (st1, cfg) tg
+  match program with
+  | [] -> scfg
+  | BINOP(op) :: rest ->
+    (match stack with
+     | rhs :: lhs :: tail ->
+       eval env ((Language.Expr.evalBinop op lhs rhs) :: tail, cfg) rest
+     | _ -> failwith "Empty stack")
+  | CONST(value) :: rest -> eval env (value :: stack, cfg) rest
+  | READ :: rest ->
+    (match input with
+     | head :: tail -> eval env (head :: stack, (state, tail, output)) rest
+     | [] -> failwith "Empty stack")
+  | WRITE :: rest -> 
+    (match stack with
+     | head :: tail -> eval env (tail, (state, input, output @ [head])) rest
+     | [] -> failwith "Empty stack")
+  | LD(var) :: rest -> eval env ((state var) :: stack, cfg) rest
+  | ST(var) :: rest ->
+    (match stack with
+     | head :: tail ->
+       eval env (tail, (Language.Expr.update var head state, input, output)) rest
+     | [] -> failwith "Empty stack")
+  | LABEL(_) :: rest -> eval env scfg rest
+  | JMP(l) :: rest -> eval env scfg (env#labeled l)
+  | CJMP(jumpOnZero, l) :: rest ->
+    (match stack with
+     | head :: tail -> if (Expr.bool_from_int head) != jumpOnZero
+                       then eval env scfg (env#labeled l)
+                       else eval env scfg rest
+     | [] -> failwith "Empty stack")
 (* Top-level evaluation
 
      val run : prg -> int list -> int list
